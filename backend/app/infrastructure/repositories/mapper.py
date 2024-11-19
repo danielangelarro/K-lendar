c37d18de2c42d5c9d93f8f9a24677ba37df0cd65
Daniel Ángel <danielarromoreno@gmail.com>
2024-11-19 07:55:39 -0500

    fix user_router response

--- backend/app/infrastructure/repositories/mapper.py
+++ backend/app/infrastructure/repositories/mapper.py
@@ -5,15 +5,15 @@ from app.infrastructure.postgresql.tables import User
 
 
 class UserMapper(BaseMapper):
-    def to_table(user_create: UserCreate) -> User:
+    def to_table(self, user_create: UserCreate) -> User:
         return User(
             username=user_create.username,
             email=user_create.email,
-            password=user_create.password,
+            password_hash=user_create.password,
             role=user_create.role
         )
 
-    def to_entity(user: User) -> UserResponse:
+    def to_entity(self, user: User) -> UserResponse:
         return UserResponse(
             username=user.username,
             email=user.email,
